4646be9f52eb814295e25c49ab9edaacc1017c1b
kojy <minami.jiro.87@gmail.com>
2024-05-26 19:32:57 +0100

    no comment

--- yolo/example.transformer.py
+++ yolo/example.transformer.py
@@ -1,10 +1,11 @@
 from transformers import YolosImageProcessor, YolosForObjectDetection
-from PIL import Image
+from PIL import Image,ImageDraw
 import torch
 import requests
+import matplotlib.pyplot as plt
 
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
+url = '../samples/zidane.jpg'#"http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(url)#requests.get(url, stream=True).raw)
 
 model = YolosForObjectDetection.from_pretrained('hustvl/yolos-tiny')
 image_processor = YolosImageProcessor.from_pretrained("hustvl/yolos-tiny")
@@ -26,3 +27,19 @@ for score, label, box in zip(results["scores"], results["labels"], results["boxe
         f"Detected {model.config.id2label[label.item()]} with confidence "
         f"{round(score.item(), 3)} at location {box}"
     )
+    # Draw bounding box on the image
+    draw = ImageDraw.Draw(image)
+    draw.rectangle(box, outline="red", width=3)
+    
+    # opencv
+    # import cv2
+    # # Draw bounding box on the image
+    # cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
+    # # Convert BGR to RGB for displaying with matplotlib
+    
+# image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# Display the image with the bounding box
+plt.imshow(image)
+plt.axis('off')  # Turn off axis numbers
+plt.show()
